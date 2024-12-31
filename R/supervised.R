f76814d338de1ece66921f46d3cbc5f028a3b1ba
stejat98 <stejat@icloud.com>
2024-12-31 02:01:51 -0500

    updated with group lasso instead of RuleFit

--- R/supervised.R
+++ R/supervised.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(pre)
+library(glmnet)
 
 # Function to load and parse varselect configuration file
 varselect <- function(varselect_config_path) {
@@ -39,35 +40,37 @@ filter_columns <- function(data) {
 #  numeric_data <- data[, sapply(data, is.numeric), drop = FALSE]
   return(data)
 }
-#' Fit RuleFit Model and Generate Output Table
+#' Fit Group lasso Model and Generate Output Table
 #'
-#' This function fits a RuleFit model to an input dataset and generates a table of model coefficients.
+#' This function fits a Group lasso model to an input dataset and generates a table of model coefficients.
 #'
-#' @param input_data_list A list of data frames to apply the RuleFit model to.
+#' @param input_data_list A list of data frames to apply the Group lasso model to.
 #' @param varselect A named list of variable sets to subset data by before modeling. Default is 'all' variables.
+#' @param dependent_variable The dependent variable for the Group lasso model (required).
 #' @param family The family of the model. Default is "gaussian".
-#' @param dependent_variable The dependent variable for the RuleFit model (required).
-#' @param max_depth The maximum depth of the tree used to generate in the RuleFit model. Default is 10.
-#' @return A list of data frames containing RuleFit model coefficients for each dataset and variable set.
+#' @param group_mapping The mapping of variables to a group.
+#' @param alpha The alpha value. Default is "1".
+#' @return A list of data frames containing Group lasso model coefficients for each dataset and variable set.
 #' @export
-supervised_rulefit <- function(input_data_list, varselect = list(all = 'all'), dependent_variable, 
-                           family = "gaussian", ad.penalty = "lambda.min", max_depth = 3) {
+
+group_lasso <- function(input_data_list, varselect = list(all = 'all'), dependent_variable, 
+                        family = "gaussian", group_mapping, alpha = 1) {
   
   # Initialize an empty list to store results
   results_list <- list()
-
+  
   # Loop through each variable set in the varselect
   for (varset in names(varselect)) {
-
+    
     print(varset)
     
     select_columns <- varselect[[varset]]
-
+    
     # Loop through each dataset in the input data list
     for (dat in names(input_data_list)) {
-
+      
       data <- input_data_list[[dat]]
-
+      
       # Subset data based on variable set, or use all columns if varset is 'all'
       subsetted_data <- if (varset == 'all') {
         data
@@ -76,26 +79,48 @@ supervised_rulefit <- function(input_data_list, varselect = list(all = 'all'), d
         required_columns <- unique(c(select_columns, dependent_variable))
         subsetted_data <- data[, required_columns, drop = FALSE]
       }
-
+      
       # Drop all columns ending in '_imp'
       subsetted_data <- subsetted_data[, !grepl("_imp$", colnames(subsetted_data))]
-
+      
       # Convert character columns to factors
       dat_filt <- subsetted_data %>% mutate_if(is.character, as.factor)
-
-      # Fit the RuleFit model using the provided parameters
-      formula <- as.formula(paste(dependent_variable, "~ ."))
-      rule_model <- pre(formula, data = dat_filt, family = family,ad.penalty = ad.penalty,  maxdepth = max_depth)
       
-      # Extract the coefficients and add metadata
-      rule_coefficients <- coef(rule_model) %>%
-        mutate(imputation_method = dat, variable_set = varset)
+      # Prepare data for glmnet
+      X <- as.matrix(dat_filt[, colnames(dat_filt) != dependent_variable])
+      y <- dat_filt[[dependent_variable]]
       
-      # Store results in a list
-      results_list[[paste(dat, varset, sep = '__')]] <- rule_model
+      # Map variable names to groups using group_mapping
+      groups <- unname(group_mapping[colnames(X)])
+      
+      # Ensure group length matches the number of predictors
+      if (length(groups) != ncol(X)) {
+        stop("The length of 'groups' must match the number of columns in 'X'.")
+      }
+      
+      # Fit the Group Lasso model using glmnet
+      fit <- cv.glmnet(X, y, alpha = alpha, family = family, grouped = TRUE, type.measure = "mse")
+      
+      # Extract optimal lambda
+      lambda_opt <- fit$lambda.min
+      
+      # Refit model using optimal lambda
+      final_model <- glmnet(X, y, alpha = alpha, family = family, grouped = TRUE, lambda = lambda_opt)
+      
+      # Identify selected variables
+      selected_vars <- colnames(X)[coef(final_model)[-1] != 0]
+      
+      # Store results with metadata
+      results_list[[paste(dat, varset, sep = '__')]] <- list(
+        model = final_model,
+        selected_vars = selected_vars
+      )
     }
   }
-
+  
   return(results_list)
 }
+
+
+
 
